247ff5ff42b6df52b489d90564aefa867c422ea7
Artem Ivanov <arti.ivnv@gmail.com>
2026-02-19 16:39:54 +0300

    feat: finish EntityManager class

---
+++ src/EntityManager.cpp
@@ -0,0 +1,37 @@
+#include "EntityManager.hpp"
+
+void EntityManager::removeDeadEntities(EntityVec &vec)
+{
+    vec.erase(std::remove_if(vec.begin(), vec.end(), [](const auto e) { return !e->isActive(); }), vec.end());
+}
+
+void EntityManager::update()
+{
+    for (auto &e : m_entitiesToAdd)
+    {
+        m_entities.push_back(e);
+        m_entityMap[e->m_tag].push_back(e);
+    }
+
+    m_entitiesToAdd.clear();
+
+    // remove dead entities the vectore of all entities
+    removeDeadEntities(m_entities);
+}
+
+std::shared_ptr<Entity> EntityManager::addEntity(const std::string &tag)
+{
+    auto e = std::shared_ptr<Entity>(new Entity(m_totalEntities++, tag));
+    m_entitiesToAdd.push_back(e);
+    return e;
+}
+
+const EntityVec &EntityManager::getEntities()
+{
+    return m_entities;
+}
+
+const EntityVec &EntityManager::getEntities(const std::string &tag)
+{
+    return m_entityMap[tag];
+}
